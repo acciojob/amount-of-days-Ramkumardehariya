0c150e6a82565c532d21494011612f80ffe08535
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-05-31 18:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfYear(year){
+	if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+
+let num = daysOfYear(2000);
+console.log(num);
+
